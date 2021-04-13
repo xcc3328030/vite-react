79c6f7e1ae08ef3d60d3436064b7f648fd950c10
xcc3328030 <414069145@qq.com>
2021-04-13 18:23:25 +0800

    123

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    plugins: [reactRefresh()],
+    css: {
+        preprocessorOptions: {
+            less: {
+                // 支持内联 JavaScript
+                javascriptEnabled: true
+                // 重写 less 变量，定制样式
+                // modifyVars: {
+                //     '@primary-color': 'red'
+                // }
+            }
+        }
+    }
 })
