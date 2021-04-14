53ce9dd00b7206d78c086377315d74bcacb7bde2
xcc3328030 <414069145@qq.com>
2021-04-14 18:36:22 +0800

    122

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,33 @@ import List from './compontents/List'
 import Footer from './compontents/Footer'
 import './index.css'
 
+  interface Todo {
+    id: number
+    name: string
+    isdo: boolean
+}
 class App extends Component {
+ 
+    // // react
+    // const [todos] = useState<Todo[]>([]);
+
+    // // 初始化
+    // useEffect(() => {
+    // // 获取业务类型
+    //     const todos: ContentTableBizType[] = [
+    //         { id: 1,name: '吃饭', isdo: true },
+    //             { id: 2,name: '睡觉', isdo: true },
+    //             { id: 3,name: '打代码', isdo: false },
+    //         ];
+    // }, [])
+
     render() {
+        const { todos } = this.state
         return (
             <div className="todo-container">
                <div className="todo-wrap">
                     <Header />
-                    <List />
+                    <List todos={todos} />
                     <Footer />
                 </div>
             </div>
