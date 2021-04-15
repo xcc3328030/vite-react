651b336f2672f28b3336c39b92f71ae53848fe70
xcc3328030 <414069145@qq.com>
2021-04-15 18:36:07 +0800

    123

--- src/compontents/Item/index.tsx
+++ src/compontents/Item/index.tsx
@@ -8,17 +8,18 @@ class Item extends Component {
         this.setState({mouse: flag})
     }
 
-    handleChange = ()=>{
-
+    handleChange = (event:any,id:string)=>{
+        console.log(event.target.checked)
+        console.log(id)
     }
     
     render() {
-        const { name, done } = this.props
+        const { name, done, id } = this.props
         const { mouse } = this.state
         return (
             <li style={{backgroundColor: mouse?'#ddd':'white'}} onMouseLeave={()=>this.handleMouse(false)} onMouseEnter={()=>this.handleMouse(true)}>
                 <label>
-                    <input type="checkbox" defaultChecked={done} onChange={this.handleChange} />
+                    <input type="checkbox" defaultChecked={done} onChange={(event)=>this.handleChange(event, id)} />
                     <span>{name}</span>
                 </label>
                 <button className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
