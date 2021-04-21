dca46c89f8d9f03ceec1e7ce466f4e4f03011875
xcc3328030 <414069145@qq.com>
2021-04-21 21:23:25 +0800

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Count from './containers/Count'
-import  Person  from './containers/Person'
+// import Count from './containers/Count'
+// import  Person  from './containers/Person'
+// import LazyLoad from './compontents/LazyLoad';
+import Hooks from './compontents/Hooks';
 class App extends Component {
     render() {
         return (
             <div>
-                <Count  />
+                {/* <Count  />
                 <hr/>
                 <Person />
+                <hr/>
+                <LazyLoad /> */}
+                <hr/>
+                <Hooks />
             </div>
         );
     }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store  from './redux/store'
 import { Provider } from 'react-redux'
+import {  BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-    <Provider store={store}>
-         <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </Provider>
+    </Router>,
   document.getElementById('root')
 )
 
